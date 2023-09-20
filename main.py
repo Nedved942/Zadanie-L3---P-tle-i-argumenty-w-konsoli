9895b8229f084e96b92d05fa1e95e276888df338
nedved942 <pawelkoziol77@wp.pl>
2023-09-20 23:34:40 +0200

    Ukończenie zasadniczej części zadania wraz z obsługą błędów

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ empty_weight_packages_list = []
 total_weight = 0
 weight_single_package = 0
 packages = 1
+max_empty_weight = 0
 
 # for element in range(1, int(elements_to_send) + 1):
 #     weight_elements_list.append(input(f"Podaj wagę {element}. elementu: "))
@@ -35,29 +36,37 @@ for number_element in range(elements_to_send):
         print("Podana wartość musi być liczbą.")
         break
 
-    if weight_element < 1 or weight_element > 10:
-        break
-
     if weight_element is None or weight_element == 0:
         packages = 0
 
+    if weight_element < 1 or weight_element > 10:
+        break
+
     weight_elements_list.append(weight_element)
-    print("Lista wag elementów: ", weight_elements_list)
+    print("Lista wag elementów:", weight_elements_list)
     total_weight = weight_element + total_weight
-    print("Suma wszystkich elementów: ", total_weight)
+    print("Suma wszystkich elementów:", total_weight)
 
     weight_single_package = weight_single_package + weight_element
 
     if weight_single_package > 20:
-        weight_packages_list.append(weight_single_package - weight_element)
+        empty_weight = 20 - (weight_single_package - weight_element)
+        if empty_weight > max_empty_weight:
+            max_empty_weight = empty_weight
+            number_max_empty_weight = packages
         weight_single_package = weight_element
         packages += 1
 
-    print("Waga pojedynczej paczki: ", weight_single_package)
-    print("Waga każdej paczki: ", weight_packages_list)
+    print("Waga pojedynczej paczki:", weight_single_package)
+    print("Waga każdej paczki:", weight_packages_list)
+
+empty_weight = 20 - weight_single_package
+if empty_weight > max_empty_weight:
+    max_empty_weight = empty_weight
+    number_max_empty_weight = packages
 
 print("*** PODSUMOWANIE ***")
 print(f"Wysłana ilość paczek: {packages}.")
 print(f"Wysłano {total_weight} kg.")
-print(f"Suma pustych kilogramów: {packages * 20 - total_weight}")
-
+print(f"Suma pustych kilogramów: {packages * 20 - total_weight} kg.")
+print(f"Najwięcej pustych kilogramów ma paczka: {number_max_empty_weight} ({max_empty_weight} kg).")
